028914fc90f30bce1d2765e516b412dfcd7bf326
alibabaih <alibabaih@gmail.com>
2015-01-12 11:10:30 +0400

    spring MVC progect using annotations

--- src/main/java/springSimpleMVCExample/config/AppInit.java
+++ src/main/java/springSimpleMVCExample/config/AppInit.java
@@ -1,7 +1,33 @@
 package springSimpleMVCExample.config;
 
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
 /**
  * Created by l-vasil on 12.01.15.
  */
-public class AppInit {
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    // Этот метод должен содержать конфигурации которые инициализируют Beans
+    // для инициализации бинов у нас использовалась аннотация @Bean
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{
+                WebConfig.class
+        };
+    }
+
+    // Тут добавляем конфигурацию, в которой инициализируем ViewResolver
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+
+        return new Class<?>[]{
+                WebConfig.class
+        };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
 }
--- src/main/java/springSimpleMVCExample/config/WebConfig.java
+++ src/main/java/springSimpleMVCExample/config/WebConfig.java
@@ -1,7 +1,33 @@
 package springSimpleMVCExample.config;
 
-/**
- * Created by l-vasil on 12.01.15.
- */
-public class WebConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration //annotation says that this class is the Java Configuration
+@EnableWebMvc //this annotation allows our project using MVC
+@ComponentScan("springSimpleMVCExample") //аналогично  mvc-dispatcher-servlet.xml, говорит, где искать компоненты проект
+public class WebConfig extends WebMvcConfigurerAdapter { //унаследовавшись от этого класса мы получим возможность сконфигурировать ResourceLocations
+
+    @Override //переопределив данный метод мы сможем указать где будут лежать ресурсы нашего проекта, такие как css, image, js и другие.
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/WEB-INF/pages/**").addResourceLocations("/pages/");
+    }
+
+    @Bean //указывает на то что это инициализация бина, и он будет создан с помощью DI
+    public InternalResourceViewResolver setupViewResolver() { //аналогичная конфигурация с mvc-dispatcher-servlet.xml
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/pages/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+
+        return resolver;
+    }
+
+    //Теперь нужно зарегистрировать конфигурацию в Spring Context это нам позволит сделать наш класс AppInit
 }
